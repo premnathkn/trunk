4d7ef1d69794cb1ca7c6c13d3b1a033f918d4c6d
premnathkn@gmail.com <premnathkn@gmail.com@96e8c8b4-9c4b-11c5-54a4-ebc9eeaac7a9>
2012-08-16 06:46:52 +0000

    Resolved import consistency.

--- trunk/src/org/ieee/iwson2/mfm/view/showprogress/ShowProgressActionListener.java
+++ trunk/src/org/ieee/iwson2/mfm/view/showprogress/ShowProgressActionListener.java
@@ -5,11 +5,7 @@ package org.ieee.iwson2.mfm.view.showprogress;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
-import org.ieee.iwson2.mfm.showprogress.dataloader.XYSeriesTest;
-import org.ieee.iwson2.mfm.showprogress.dataloader.XmlIterationDataLoader;
-import org.ieee.iwson2.mfm.showprogress.model.Iteration;
 import org.ieee.iwson2.mfm.view.showprogress.chart.RepulsionChart;
 import org.jfree.ui.RefineryUtilities;
 
